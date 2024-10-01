5a61bfcc39b56d524a471a8383e8fe6e1070a236
Koreacty <48289978+wilsonsilva009@users.noreply.github.com>
2024-10-01 16:54:53 +0100

    About page redesign

---
+++ Pages/Aboutme/about.js
@@ -0,0 +1,37 @@
+let currentIndex = 0;
+
+var IsScrolling = false
+
+function detectScrollDirection(event) {
+    if (IsScrolling) return
+
+    IsScrolling = true
+
+    const delta = event.deltaY || event.detail || event.wheelDelta;
+
+    const pages = ["#page1", "#page2", "#page3"];
+
+    if (delta > 0) {
+        currentIndex++;
+    } else {
+        currentIndex--;
+    }
+
+    
+    if (currentIndex >= pages.length) {
+        currentIndex = 0;
+    } else if (currentIndex < 0) {
+        currentIndex = pages.length - 1;
+    }
+
+    document.querySelector(pages[currentIndex]).scrollIntoView({ behavior: 'smooth' });
+
+    setTimeout(function() {
+        IsScrolling = false
+    }, 500);
+    
+}
+
+
+
+window.addEventListener('wheel', detectScrollDirection);
