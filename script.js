4eb0362411216f871c52abc4f951e5f1d5995d24
Koreacty <48289978+wilsonsilva009@users.noreply.github.com>
2024-10-11 12:17:00 +0100

    Update to main page
    + Updated scrolling methods in main page (CSS and JS)
    + Updated stylings and positioning to the buttons

--- script.js
+++ script.js
@@ -33,3 +33,38 @@ window.onscroll = function() {
         phone_button.style.display = "none";
     }
 };
+
+var IsScrolling = false
+
+let currentIndex = 0;
+
+function detectScrollDirection(event) {
+    if (IsScrolling) return
+
+    IsScrolling = true
+
+    const delta = event.deltaY || event.detail || event.wheelDelta;
+
+    const images = ["#img1", "#img2", "#img3", "#img4"];
+
+    if (delta > 0) {
+        currentIndex++;
+    } else {
+        currentIndex--;
+    }
+    
+    if (currentIndex >= images.length) {
+        currentIndex = 0;
+    } else if (currentIndex < 0) {
+        currentIndex = images.length - 1;
+    }
+
+    document.querySelector(images[currentIndex]).scrollIntoView({ behavior: 'smooth' });
+
+    setTimeout(function() {
+        IsScrolling = false
+    }, 500);
+    
+}
+
+window.addEventListener('wheel', detectScrollDirection);
