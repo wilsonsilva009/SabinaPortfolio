e593ec72621267075edf45d3d0d19bd5a5038828
Koreacty <48289978+wilsonsilva009@users.noreply.github.com>
2025-01-03 01:31:22 +0000

    Update

--- Pages/Aboutme/about.js
+++ Pages/Aboutme/about.js
@@ -33,15 +33,45 @@ function detectScrollDirection(event) {
 
 window.addEventListener('wheel', detectScrollDirection);
 
-var cooldown = false;
+
 function ClickButton(buttonClicked) {
-    if (cooldown) return;
-
-
-    cooldown = true;
 
 
     buttonClicked.style.setProperty('--left-position', '125%');
+
+    switch (buttonClicked.id){
+
+        case "button1":
+            document.getElementById("description").innerHTML = "Hello! My name is Sabina, and I’m a passionate 3D Character Artist with a degree in Creative Media.<br><br>I thrive on crafting unique and immersive characters that bring stories to life. I specialize in creating high-quality assets optimized for video games.<br><br>I’m constantly seeking new challenges that push my artistic and technical boundaries. When I’m not creating characters, you’ll likely find me drawing inspiration from movies, gaming, or sketching ideas for my next project.";
+            break;
+        
+        case "button2":
+            document.getElementById("description").innerHTML = `
+            I am dedicated to continuously improving the quality and creativity of my work through intensive learning and practice. My core expertise lies in the entire character creation pipeline. This includes:<br><br>
+            
+                <ul>
+                    <li>Sculpting & Modeling: Mastery in creating detailed, high-poly sculpts and optimizing them into efficient low-poly models suitable for real-time applications</li>
+                    <li>Retopology & UV Mapping: Proficient in creating clean, production-ready topology and efficient UV layouts for seamless texturing</li>
+                    <li>Texturing & Baking: Skilled in generating realistic and stylized textures using advanced techniques, as well as baking maps for high-quality results</li>
+                    <li>Rigging & Animation Prep: Experienced in rigging characters for animation, ensuring functionality and adaptability</li>
+                    <li>Rendering: Capable of producing stunning renders to showcase characters in diverse settings, from concept showcases to in-engine previews</li>
+                </ul><br>
+
+            
+            I am also proficient in multiple game engines, including Unreal Engine 5 (UE5), Unity, and Godot, enabling seamless integration of assets into interactive environments. I am proficient in using version control systems like GitHub to collaborate with team members effectively. My adaptability allows me to tailor my work to meet specific project requirements, whether working independently or within a collaborative team.
+                `;
+            break;
+
+        case "button3":
+            document.getElementById("description").innerHTML = `
+            Find out more about me:<br><br>
+
+            sabinapawlow@gmail.com<br>
+            www.linkedin.com/in/sabina-pawłów
+            `;
+            break;
+        
+    }
 
 
     setTimeout(function() {
@@ -52,11 +82,55 @@ function ClickButton(buttonClicked) {
 
     setTimeout(function() {
         buttonClicked.style.setProperty('--opacity', '1');
-        cooldown = false;
     }, 1000);
     
 }
 
+function ShowPollaroid(){
+    const pollaroid = document.getElementById("polaroid-holder");
+    const skills_caroussel = document.getElementById("skills-caroussel");
+
+    pollaroid.style.display = "flex";
+    skills_caroussel.style.display = "none";
+}
+
+function ShowSkills(){
+    const pollaroid = document.getElementById("polaroid-holder");
+    const skills_caroussel = document.getElementById("skills-caroussel");
+
+    pollaroid.style.display = "none";
+    skills_caroussel.style.display = "flex";
+}
+
+/*
+
+var holder1_val = 0;
+var holder2_val = 0;
+var holder3_val = 0;
+var holder1_index = 1;
+var holder2_index = 1;
+var holder3_index = 1;
+function ArrowClicked(arrowClicked) {
+    var holder = arrowClicked.parentElement.parentElement.id;
+    var direction = arrowClicked.id;
+    var imgcontainer = document.querySelector("#" + holder + ">.holder-imgs");
+    var size = imgcontainer.children.length;
+
+
+    if (direction == "right"){
+        if (holder3_index == size - 2) return;
+        holder3_index += 1;
+        holder3_val -= 190;
+    }else{
+        if (holder3_index == 1) return;
+        holder3_index -= 1;
+        holder3_val += 190;
+    }
+
+    imgcontainer.style.transform = `translateX(${holder3_val}px)`;
+}
+    
+*/
 
 
 
